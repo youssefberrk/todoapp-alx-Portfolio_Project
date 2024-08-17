9a34fc3ef49f4280ac2443737a4dd7ee9bc54d06
youssef <yberrakouan.lol@gmail.com>
2024-08-17 13:51:48 +0100

    Create TodoItem component for individual task management

---
+++ todoapp/src/components/TodoItem.jsx
@@ -0,0 +1,33 @@
+import styles from "./todoitem.module.css";
+
+export default function TodoItem({ item, todos, setTodos }) {
+  function handleDelete(item) {
+    console.log("Delete button clicked for item", item);
+    setTodos(todos.filter((todo) => todo !== item));
+  }
+  function handleClick(name) {
+    const newArray = todos.map((todo)=>
+        todo.name === name ? {...todo, done: !todo.done } : todo
+    )
+    setTodos(newArray);
+    
+  }
+  const className = item.done ? styles.completed : ""
+  return (
+    <div className={styles.item}>
+      <div className={styles.itemName}>
+        <span className={className} onClick={() => handleClick(item.name)}>{item.name}</span>
+
+        <span>
+          <button
+            onClick={() => handleDelete(item)}
+            className={styles.deleteButton}
+          >
+            x
+          </button>
+        </span>
+      </div>
+      <hr className={styles.line} />
+    </div>
+  );
+}
